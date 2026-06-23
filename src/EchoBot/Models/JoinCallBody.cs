0ba2373a1415de73a735c4c0c9e2334544b3a71b
y4m4s <yamas.yamashita@gmail.com>
2026-06-23 23:46:07 +0000

    Import Microsoft Graph EchoBot sample for DeciScope

---
+++ src/EchoBot/Models/JoinCallBody.cs
@@ -0,0 +1,48 @@
+﻿// ***********************************************************************
+// Assembly         : EchoBot.Models
+// Author           : JasonTheDeveloper
+// Created          : 09-07-2020
+//
+// Last Modified By : bcage29
+// Last Modified On : 10-27-2023
+// ***********************************************************************
+// <copyright file="JoinCallBody.cs" company="Microsoft">
+//     Copyright ©  2023
+// </copyright>
+// <summary></summary>
+// ***********************************************************************
+namespace EchoBot.Models
+{
+    /// <summary>
+    /// The join call body.
+    /// </summary>
+    public class JoinCallBody
+    {
+        /// <summary>
+        /// Gets or sets the Teams meeting join URL.
+        /// </summary>
+        /// <value>The join URL.</value>
+        public string JoinUrl { get; set; }
+
+        /// <summary>
+        /// Gets or sets the tenant id.
+        /// Required when using short meeting URLs (e.g. https://teams.microsoft.com/meet/...).
+        /// The tenant id cannot be inferred from short URLs and must be provided separately.
+        /// </summary>
+        /// <value>The tenant id.</value>
+        public string? TenantId { get; set; }
+
+        /// <summary>
+        /// Gets or sets the display name.
+        /// Teams client does not allow changing of ones own display name.
+        /// If display name is specified, we join as anonymous (guest) user
+        /// with the specified display name.  This will put bot into lobby
+        /// unless lobby bypass is disabled.
+        /// Side note: if display name is specified, the bot will not have
+        /// access to UnmixedAudioBuffer in the Skype Media libraries.
+        /// </summary>
+        /// <value>The display name.</value>
+        public string? DisplayName { get; set; }
+    }
+}
+
